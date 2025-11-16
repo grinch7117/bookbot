d6182362fe434c86c4eeff1a0243367144d44c26
Alex Bihori <alexandru.bihori@sonnenklar.tv>
2025-11-16 09:42:25 +0100

    Add main functions

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(path):
+    contents = ''
+    with open(path) as f:
+        contents = f.read()
+    return contents
+
+def main():
+    book_content = get_book_text("books/frankenstein.txt")
+    print(book_content)
+
+main()
