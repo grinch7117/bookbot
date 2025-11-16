63faa085a069e4e806034a77d5372fe91b00a4ff
Alex Bihori <alexandru.bihori@sonnenklar.tv>
2025-11-16 09:44:33 +0100

    Add method for counting words

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ def get_book_text(path):
     contents = ''
     with open(path) as f:
         contents = f.read()
+
     return contents
+
+def count_words(book_text):
+    words = book_text.split()
+    return len(words)
 
 def main():
     book_content = get_book_text("books/frankenstein.txt")
-    print(book_content)
+    word_count = count_words(book_content)
+
+    print(f"Found {word_count} total words")
 
 main()
