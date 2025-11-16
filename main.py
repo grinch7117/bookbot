ed0c08b13462c1aaedc7c206211370f59e5a4452
Alex Bihori <alexandru.bihori@sonnenklar.tv>
2025-11-16 17:33:01 +0100

    Final touches

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import count_words, count_characters, generate_character_report
 
 
@@ -9,7 +11,11 @@ def get_book_text(path):
     return contents
 
 def main():
-    book_content = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_content = get_book_text(sys.argv[1])
     word_count = count_words(book_content)
     character_report = generate_character_report(count_characters(book_content))
 
